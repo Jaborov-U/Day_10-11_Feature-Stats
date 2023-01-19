23caf19cfdca628ce70dd241ccf7a8b12b870802
Jaborov-U <umed.jaborov.zz@gmail.com>
2023-01-19 18:47:04 +0500

    создана тело функции AVG

--- pkg/bank/stats/stats.go
+++ pkg/bank/stats/stats.go
@@ -4,5 +4,12 @@ import "github.com/Jaborov-U/Day_10-11_Feature-Stats/pkg/bank/types"
 
 // AVG рассчитывает среднюю сумму платежа.
 func AVG(payments []types.Payment) types.Money {
-	
+
+	summPays := 0
+
+	for _, payment := range payments {
+		summPays += int(payment.Amount)
+	}
+
+	return types.Money(summPays / len(payments))
 }
---
+++ pkg/bank/stats/stats_test.go
@@ -0,0 +1,7 @@
+package stats
+
+func Example() {
+	
+	//Output:
+	
+}
