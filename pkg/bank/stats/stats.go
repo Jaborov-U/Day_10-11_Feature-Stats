49ac4ed3ffa709a8e17a4c89a5a67534e400bd3b
Jaborov-U <umed.jaborov.zz@gmail.com>
2023-01-19 18:36:23 +0500

    создана оболочка функции AVG

---
+++ pkg/bank/stats/stats.go
@@ -0,0 +1,8 @@
+package stats
+
+import "github.com/Jaborov-U/Day_10-11_Feature-Stats/pkg/bank/types"
+
+// AVG рассчитывает среднюю сумму платежа.
+func AVG(payments []types.Payment) types.Money {
+	
+}
